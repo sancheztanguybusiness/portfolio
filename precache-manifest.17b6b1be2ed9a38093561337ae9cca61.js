70ba0af0ad9aafafb9ad3637363a25019fc08c01
sancheztanguybusiness <sanchez.tanguy.business@gmail.com>
2019-02-11 17:18:37 +0100

    Updates

---
+++ precache-manifest.17b6b1be2ed9a38093561337ae9cca61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c13d38d822779b61f970",
+    "url": "/portfolio/static/css/main.a6c0adca.chunk.css"
+  },
+  {
+    "revision": "c13d38d822779b61f970",
+    "url": "/portfolio/static/js/main.c13d38d8.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/portfolio/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "bd47b6d43e5531cb031b3de466fe8b92",
+    "url": "/portfolio/static/media/logo.bd47b6d4.png"
+  },
+  {
+    "revision": "50ff29507147fa459fdbb0e568c9aecd",
+    "url": "/portfolio/index.html"
+  }
+];
